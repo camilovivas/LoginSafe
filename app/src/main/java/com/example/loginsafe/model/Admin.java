6831acb9f0313285df656d5c37e516a3d3efc7eb
johnkennedyls <jkls1998@gmail.com>
2022-05-30 20:45:42 -0500

    Adding user, admin details

--- app/src/main/java/com/example/loginsafe/model/Admin.java
+++ app/src/main/java/com/example/loginsafe/model/Admin.java
@@ -1,5 +1,26 @@
 package com.example.loginsafe.model;
 
 public class Admin {
-    //usuario y contraseña
+    private String user;
+    private String password;
+
+    public Admin(){
+
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- app/src/main/java/com/example/loginsafe/model/User.java
+++ app/src/main/java/com/example/loginsafe/model/User.java
@@ -1,6 +1,39 @@
 package com.example.loginsafe.model;
 
+import java.util.Calendar;
+
+
 public class User {
 //usuario unico, contraseña, ultima hora de ingreso
+    private String userName;
+    private String password;
+    private Calendar lastCheckInTime;
 
+    public User(){
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Calendar getLastCheckInTime() {
+        return lastCheckInTime;
+    }
+
+    public void setLastCheckInTime(Calendar lastCheckInTime) {
+        this.lastCheckInTime = lastCheckInTime;
+    }
 }
