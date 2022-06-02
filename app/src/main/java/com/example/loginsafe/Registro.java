6223cdddc2997a9a795a543495fc73cc17803593
johnkennedyls <jkls1998@gmail.com>
2022-06-02 06:22:00 -0500

    updating pbkdf2

--- app/src/main/java/com/example/loginsafe/Registro.java
+++ app/src/main/java/com/example/loginsafe/Registro.java
@@ -6,15 +6,32 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 
+
 import com.example.loginsafe.model.User;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
+
+import java.security.SecureRandom;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.SecretKeyFactory;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
 public class Registro extends AppCompatActivity {
 
     private TextInputEditText username, password, repassword;
     private Button registro;
+
+    private static final int saltBytes = 24;
+    private static final int hashBytes = 24;
+    private static final int pbkdf2Iterations = 3000;
+
+    private static final int iterationIndex = 0;
+    private static final int saltIndex = 1;
+    private static final int pbkdf2Index = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +47,7 @@ public class Registro extends AppCompatActivity {
             String name = username.getText().toString();
             String pass = password.getText().toString();
             String repass = repassword.getText().toString();
-            User user = new User(name, pass, 0L);//TODO esa pass se teine que cambiar por la hash
+            User user = new User(name, generatePassword(pass), 0L);//TODO esa pass se teine que cambiar por la hash
             if(pass.equals(repass)){//si no son iguales las contraseñas
                 Query query = FirebaseFirestore.getInstance().collection("users");
                 if(query.get().getResult().isEmpty()){// si la collectios esta vacia
@@ -58,7 +75,27 @@ public class Registro extends AppCompatActivity {
     }
 
     public String generatePassword(String pass){
-        //TODO
-        return null;
+        return createHash(pass.toCharArray());
+
     }
+
+    private String createHash(char[] password) {
+        // Generating random salt
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[saltBytes];
+        random.nextBytes(salt);
+
+        // Hashing the password
+        byte[] hash = pbkdf2(password, salt, pbkdf2Iterations, hashBytes);
+        // format  for return iterations:salt:hash
+        return pbkdf2Iterations + ":" + toHex(salt) + ":" +  toHex(hash);
+    }
+
+    private  byte[] pbkdf2(char[] password, byte[] salt, int iterations, int bytes)
+
+        PBEKeySpec spec = new PBEKeySpec(password, salt, iterations, bytes * 8);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance(PBKDF2_ALGORITHM);
+        return skf.generateSecret(spec).getEncoded();
+    }
+
 }
